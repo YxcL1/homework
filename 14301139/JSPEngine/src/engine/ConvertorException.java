a84c9035ea7d07f3951354e194eafe771024c9c4
YxcL1 <14301139@bjtu.edu.cn>
2016-10-23 18:51:44 +0800

    Add files via upload

---
+++ 14301139/JSPEngine/src/engine/ConvertorException.java
@@ -0,0 +1,14 @@
+package engine;
+
+public class ConvertorException extends Exception {
+
+	/**
+	 * code = 0, ≈‰∂‘¥ÌŒÛ
+	 */
+	public int code;
+
+	public ConvertorException(int code) {
+		this.code = code;
+	}
+
+}
---
+++ 14301139/JSPEngine/src/engine/Printable.java
@@ -0,0 +1,8 @@
+package engine;
+
+
+public interface Printable {
+
+	public void print();
+
+}
